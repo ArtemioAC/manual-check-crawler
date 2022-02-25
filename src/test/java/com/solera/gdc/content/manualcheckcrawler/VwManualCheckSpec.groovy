73058c87a1acada314402127dc2d8a4928bfb856
Alvarado <artemio.alvarado@audatex.com>
2022-02-25 11:56:01 -0600

    Fixed unit tests

--- src/test/java/com/solera/gdc/content/manualcheckcrawler/VwManualCheckSpec.groovy
+++ src/test/java/com/solera/gdc/content/manualcheckcrawler/VwManualCheckSpec.groovy
@@ -1,18 +1,19 @@
 package com.solera.gdc.content.manualcheckcrawler
 
-import com.solera.gdc.content.manualcheckcrawler.crawler.VwManualCheckService
+import com.solera.gdc.content.manualcheckcrawler.service.ManualCheckService
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
 import spock.lang.Specification
 
 class VwManualCheckSpec extends Specification{
-    VwManualCheckService systemUnderTest
+    ManualCheckService systemUnderTest
     WebDriver mockWebDriver = Mock(WebDriver)
     WebElement mockWebElement = Mock(WebElement)
 
     def setup() {
-        systemUnderTest = new VwManualCheckService(mockWebDriver)
+        systemUnderTest = new ManualCheckService("VW", "some-url",
+                "someUsername", "somePassword", mockWebDriver)
         mockWebDriver.findElement(_ as By) >> mockWebElement
     }
 
